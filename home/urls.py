f464d8ee780d8593553628ea8294575e6a4d0226
Gabriela Alvarez <galva041@ucr.edu>
2022-04-17 14:43:49 -0700

    search titles

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,6 @@ from . import views
 #URLConf (url configuration)
 urlpatterns = [
     path('', views.home, name="home"), #always end routes w/ forward slash
-    path('search', views.search_bar, name="search-bar")
-    #path('homepage/', views.search_bar)
+    path('search', views.search_bar, name="search-bar"),
+    path('search_games/', views.search_games, name="search-games")
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from Games.models import Games
+
 # Create your views here.
 # request handler
+def search_games(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        games = Games.objects.filter(title__contains=searched)
+        return render(request, 'home/search_games.html', {'searched':searched, 'games':games})
+    else: 
+        return render(request, 'home/search_games.html', {})
 
 def home(request):
     return render(request, 'home/home.html', {'name': 'CS Girlie'})
