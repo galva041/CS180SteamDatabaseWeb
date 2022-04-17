875d373f549c464d303a249cafa48184f22496f3
condoes <cindy.do10@gmail.com>
2022-04-16 22:06:31 -0700

    added navbar

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 #URLConf (url configuration)
 urlpatterns = [
-    path('hello/', views.say_hello) #always end routes w/ forward slash
+    path('', views.home, name="home"), #always end routes w/ forward slash
     #path('homepage/', views.search_bar)
 ]
--- home/views.py
+++ home/views.py
@@ -4,8 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 # request handler
 
-def say_hello(request):
-    return render(request, 'hello.html', {'name' : 'CS Girlies'})
-
-def search_bar(request):
-    return render(request, 'homepage.html')
+def home(request):
+    return render(request, 'home/home.html', {})
