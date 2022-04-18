50360e80c4f3d229e1c2bb94ef057777f691fd6c
unknown <apham125@ucr.edu>
2022-04-17 18:45:08 -0700

    added all games and improved search

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from . import views
 #URLConf (url configuration)
 urlpatterns = [
     path('', views.home, name="home"), #always end routes w/ forward slash
-    path('search', views.search_bar, name="search-bar"),
-    path('search_games/', views.search_games, name="search-games")
+    #path('search', views.search_bar, name="search-bar"),
+    path('search_games/', views.search_games, name="search-games"),
+    path('allGames/',views.allGames, name="allGames" )
 ]
