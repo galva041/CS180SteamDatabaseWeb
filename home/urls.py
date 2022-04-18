8d61553fcbf380c6653490460c69683c26fd904f
unknown <apham125@ucr.edu>
2022-04-17 20:17:47 -0700

    removed redundant files and code

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"), #always end routes w/ forward slash
     #path('search', views.search_bar, name="search-bar"),
-    path('search_games/', views.search_games, name="search-games"),
-    path('allGames/',views.allGames, name="allGames" )
+    path('searchGames/', views.search_games, name="search-games"),
+    path('allGames/', views.all_games, name="all-games" )
 ]
--- home/views.py
+++ home/views.py
@@ -4,33 +4,15 @@ from django.http import HttpResponse
 from Games.models import Games
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-# Create your views here.
-# request handler
-def search_games(request):
-    if request.method == "POST":
-        searched = request.POST['searched']
-        if searched:
-            games = Games.objects.filter(title__contains=searched) or Games.objects.filter(dev__contains=searched) or Games.objects.filter(publisher__contains=searched)
-            return render(request, 'home/search_games.html', {'searched':searched, 'games':games})
-            #return render(request, 'home/allGames.html', {'searched':searched, 'games':games})
-        else:
-            return allGames(request)
-    else: 
-        return render(request, 'home/search_games.html', {})
 
 def home(request):
     return render(request, 'home/home.html', {'name': 'CS Girlie'})
 
-#def search_bar(request):
-    #if request.method == "POST":
-    #return render(request, 'home/search_bar.html', {})
-   # else:
-        #return render(request, 'home/search_bar.html', {})
-
-def allGames(request):
+def all_games(request):
+    searched = ''
     games_list = Games.objects.all()
     page = request.GET.get('page', 1)
-
+                
     paginator = Paginator(games_list, 25)
     try:
         games = paginator.page(page)
@@ -39,5 +21,16 @@ def allGames(request):
     except EmptyPage:
         games = paginator.page(paginator.num_pages)
 
-    return render(request, 'home/allGames.html', {'games': games})
-    
+    return render(request, 'home/DisplayGames.html', {'':searched,'games': games})
+
+def search_games(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        if searched:
+            games = Games.objects.filter(title__contains=searched) or Games.objects.filter(dev__contains=searched) or Games.objects.filter(publisher__contains=searched)
+            return render(request, 'home/DisplayGames.html', {'searched':searched, 'games':games})
+        else:
+            return all_games(request)
+    else:
+        return all_games(request)
+   
