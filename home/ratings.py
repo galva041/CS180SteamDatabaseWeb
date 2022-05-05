c0899f9463d04b5092e31420cd639c1c7c5bb1b0
shrimpnoodles <hannahcierlak@gmail.com>
2022-05-05 13:09:46 -0700

    added low and high rating functionality

---
+++ home/ratings.py
@@ -0,0 +1,16 @@
+class GoodRatings:
+    title = ""
+    pos_rate = 0
+
+    def __init__(self, title, pos_rate):
+        self.title = title
+        self.pos_rate = pos_rate
+
+class BadRatings:
+    title = ""
+    neg_rate = 0
+
+    def __init__(self, title, neg_rate):
+        self.title = title
+        self.neg_rate = neg_rate
+        
